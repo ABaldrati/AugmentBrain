6b5c4e42ae40e1c648aa1a87b583185cf672be62
Alberto Baldrati <a_baldrati@hotmail.it>
2021-05-08 23:38:50 +0200

    docs(gan): add references on gan code

--- src/GAN.py
+++ src/GAN.py
@@ -29,61 +29,9 @@ session = tf.compat.v1.InteractiveSession(config=config)
 def build_discriminator(Chans=8, Samples=250,
                         dropoutRate=0.5, kernLength=125, F1=12,
                         D=2, F2=24, norm_rate=0.25, dropoutType='Dropout', use_sigmoid_activation=True):
-    """ Keras Implementation of EEGNet
-    http://iopscience.iop.org/article/10.1088/1741-2552/aace8c/meta
-    Note that this implements the newest version of EEGNet and NOT the earlier
-    version (version v1 and v2 on arxiv). We strongly recommend using this
-    architecture as it performs much better and has nicer properties than
-    our earlier version. For example:
-
-        1. Depthwise Convolutions to learn spatial filters within a
-        temporal convolution. The use of the depth_multiplier option maps
-        exactly to the number of spatial filters learned within a temporal
-        filter. This matches the setup of algorithms like FBCSP which learn
-        spatial filters within each filter in a filter-bank. This also limits
-        the number of free parameters to fit when compared to a fully-connected
-        convolution.
-
-        2. Separable Convolutions to learn how to optimally combine spatial
-        filters across temporal bands. Separable Convolutions are Depthwise
-        Convolutions followed by (1x1) Pointwise Convolutions.
-
-
-    While the original paper used Dropout, we found that SpatialDropout2D
-    sometimes produced slightly better results for classification of ERP
-    signals. However, SpatialDropout2D significantly reduced performance
-    on the Oscillatory dataset (SMR, BCI-IV Dataset 2A). We recommend using
-    the default Dropout in most cases.
-
-    Assumes the input signal is sampled at 128Hz. If you want to use this model
-    for any other sampling rate you will need to modify the lengths of temporal
-    kernels and average pooling size in blocks 1 and 2 as needed (double the
-    kernel lengths for double the sampling rate, etc). Note that we haven't
-    tested the model performance with this rule so this may not work well.
-
-    The model with default parameters gives the EEGNet-8,2 model as discussed
-    in the paper. This model should do pretty well in general, although it is
-	advised to do some model searching to get optimal performance on your
-	particular dataset.
-    We set F2 = F1 * D (number of input filters = number of output filters) for
-    the SeparableConv2D layer. We haven't extensively tested other values of this
-    parameter (say, F2 < F1 * D for compressed learning, and F2 > F1 * D for
-    overcomplete). We believe the main parameters to focus on are F1 and D.
-    Inputs:
-
-      nb_classes      : int, number of classes to classify
-      Chans, Samples  : number of channels and time points in the EEG data
-      dropoutRate     : dropout fraction
-      kernLength      : length of temporal convolution in first layer. We found
-                        that setting this to be half the sampling rate worked
-                        well in practice. For the SMR dataset in particular
-                        since the data was high-passed at 4Hz we used a kernel
-                        length of 32.
-      F1, F2          : number of temporal filters (F1) and number of pointwise
-                        filters (F2) to learn. Default: F1 = 8, F2 = F1 * D.
-      D               : number of spatial filters to learn within each temporal
-                        convolution. Default: D = 2
-      dropoutType     : Either SpatialDropout2D or Dropout, passed as a string.
+    """
+        GAN discriminator based on EEGNet
+        http://iopscience.iop.org/article/10.1088/1741-2552/aace8c/meta
     """
 
     if dropoutType == 'SpatialDropout2D':
@@ -245,6 +193,10 @@ def build_cgan_generator(z_dim, num_classes=3):
 
 
 class GAN(keras.Model):
+    """
+        GAN code heavily based on https://keras.io/examples/generative/dcgan_overriding_train_step/
+    """
+
     def __init__(self, discriminator, generator, latent_dim):
         super(GAN, self).__init__()
         self.discriminator = discriminator
@@ -320,6 +272,10 @@ class GAN(keras.Model):
 
 
 class WGAN(keras.Model):
+    """
+        WGAN code heavily based on https://keras.io/examples/generative/wgan_gp/
+    """
+
     def __init__(
             self,
             discriminator,
